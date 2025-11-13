ad41f31e019563da778f6c1b78cbd3d39cab6957
Jady Tippetts <jadytippetts@mail.weber.edu>
2025-11-12 17:33:20 -0700

    Add feedback message adn clean output to pulse.sh

--- pulse.sh
+++ pulse.sh
@@ -4,4 +4,7 @@
 git log --oneline | wc -l > pulse.txt
 curl -s "http://worldclockapi.com/api/json/utc/now" \
   | sed -n 's/.*"currentDateTime":"\([^"]*\)".*/\1/p' >> pulse.txt
+echo "" >> pulse.txt
+git ls-files | wc -l >> pulse.txt
 cat pulse.txt
+echo "✅ Data written to pulse.txt"
