ccfa222c0e14ebebb2fd316d6e7d1f9422f9db52
Jady Tippetts <jadytippetts@mail.weber.edu>
2025-11-18 12:25:09 -0700

    Reflection added to test_pulse_tools.py file

--- test_pulse_tools.py
+++ test_pulse_tools.py
@@ -1,4 +1,9 @@
+# AI suggested testing `read_pulse_file` because it reads and processes external data,
+# which is a key part of the pipeline. I created a unit test to make sure the function
+# reads at least three lines from `pulse.txt`.
+
 from pulse_tools import read_pulse_file
 
 def test_read_pulse_file():
     assert read_pulse_file("pulse.txt") >= 3
+
